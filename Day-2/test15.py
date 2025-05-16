002e1e65607aab4f367c623c2aff3d72d831b568
thachayani-QA <thachimano31@gmail.com>
2025-05-16 14:58:25 +0530

    Create test15.py

---
+++ Day-2/test15.py
@@ -0,0 +1,25 @@
+# str = "thachi"
+# str[0] = "A"
+
+# Slicing
+
+my_str = "I am learning Python from the TheTestingAcademy"
+print(my_str[0:4])
+#[start:end:step]
+print(my_str[8:len(my_str)])
+print(my_str[::-1])
+
+
+# list - Collectio of items
+# shopping list
+my_list = ["milk","oraganes","apple"]
+print(my_list)
+print(my_list[0])
+print(my_list[2])
+print(len(my_list))
+#print(my_list[3]) #  list index out of range
+
+
+String1 = "Hello, I'm a Pramod"
+list1 = list(String1)
+print(list1)
