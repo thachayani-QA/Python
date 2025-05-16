b1e4984fe230750029eb5906db1cce95e2a98804
thachayani-QA <thachimano31@gmail.com>
2025-05-16 15:01:17 +0530

    Create file1.py

---
+++ Day-3/file1.py
@@ -0,0 +1,17 @@
+# Condition and Loop
+
+age = int(input("Enter the Your AGE!"))
+
+if age > 18:
+    print("Yes You can watch the Movie(Adult)")
+    
+else:
+    print("Not Allowed to Watch the Movie")
+
+
+
+age = 29
+if age > 28:
+    print("You are allowed to go the club. Goa")
+else:
+    print("You are not allowed!")
